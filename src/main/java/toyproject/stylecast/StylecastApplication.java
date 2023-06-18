7c0ca9ef14692819b8bfa40c52fe4a6f9ff106b7
jaemin-shin02 <sour_jam0220@naver.com>
2023-06-19 00:38:09 +0900

    test commit

---
+++ src/main/java/toyproject/stylecast/StylecastApplication.java
@@ -0,0 +1,13 @@
+package toyproject.stylecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StylecastApplication.class, args);
+	}
+
+}
---
+++ src/main/java/toyproject/stylecast/test.java
@@ -0,0 +1,10 @@
+package toyproject.stylecast;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class test {
+    private String name;
+}
